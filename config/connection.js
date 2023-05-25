9f01c8e42632850650779439816eadf9bfdc9298
Mohamed <amiiini44@hotmail.com>
2023-05-25 15:51:28 -0500

    deployment

--- config/connection.js
+++ config/connection.js
@@ -20,9 +20,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+      host: "0.0.0.0",
+      dialect: "mysql",
+      port: 3306,
     }
   );
 }
